a12a48f2e5e06764982d312ec6e4c534650c2a47
Klaus Deiss <k.deiss@it-userdesk.de>
2023-03-20 21:49:09 +0100

    cleanup

--- afterpull.sh
+++ afterpull.sh
@@ -3,3 +3,22 @@ touch autoadb/autoadb.txt
 touch fc/DEVICE.cfg
 touch fc/LOCAL_DEVICES.cfg
 touch fc/STATIC_DEVICES.cfg
+
+
+for a in `find .`
+do
+    chown nodered:nodered $a -v
+done
+
+for d in `find . -type d`
+do
+    chmod 770 $d -v
+done 
+
+
+for f in `find "fc/media" -type f`
+do
+    ls -la $f
+    chmod 660 $f -v
+    ls -la $f
+done 
